294bca7aa068b62ca19ffd54619fba85327ed6af
Sobirrr <sobirtag@gmail.com>
2021-08-26 11:38:01 +0500

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 $(function(){
+
   $(".rate-star").rateYo({
     rating: 5,
-    startWidth: "12px",
+    startWidth: "1px",
     readOnly: true,
   });
 
@@ -12,6 +13,25 @@ $(".product-slider__inner").slick({
   slidesToScroll: 4,
 });
 
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  prefix: "$"
+});
+
+$(".icon-th-list").on("click", function(){
+  $(".products__item").addClass("list");
+  $(".icon-th-list").addClass("active");
+  $(".icon-th-large").removeClass("active");
+});
+$(".icon-th-large").on("click", function(){
+  $(".products__item").removeClass("list");
+  $(".icon-th-large").addClass("active");
+  $(".icon-th-list").removeClass("active");
+});
 
   var mixer = mixitup('.products__inner-box');
 })
